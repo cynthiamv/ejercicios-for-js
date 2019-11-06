db43f4ce7d6fabeeeed90749c2a539b51c71962e
cynthia.mvico <cynthia.mvico@gmail.com>
2019-11-06 18:00:57 -0300

    ejercicios del 1 al 3

---
+++ javavscript.js
@@ -0,0 +1,19 @@
+// 1. Mostrar en consola los números del 0 al 10
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// 2. Mostrar en consola los números del 100 al 0
+
+for (let i = 100; i >= 0; i--) {
+    console.log(i);
+}
+
+
+// 3. Mostrar en consola los números pares del 0 al 1000 
+
+for (let i = 0; i <= 1000; i += 2) {
+    console.log(i);
+}
